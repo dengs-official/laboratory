3af8f52280182837e611f11673be642957de0660
dengs.official <dengs.official@gmail.com>
2021-05-07 22:45:38 +0800

    feat: 添加ref模版引用

---
+++ Vue.js@3.x/src/views/CAPIRefTemp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="root">This is a root element </div>
+</template>
+
+<script>
+import { ref, watchEffect } from 'vue';
+
+export default {
+  name: 'CAPIRefTemp',
+  setup() {
+    const root = ref(null);
+    watchEffect(() => {
+      // console.log(root);
+    }, { flush: 'post' });
+
+    const count = ref(0);
+    return {
+      root,
+      count,
+    };
+  },
+  mounted() {
+    console.log(this.$refs.root === this.root);
+    console.log(this.count);
+    console.log(this);
+  },
+};
+</script>
+
+<style>
+
+</style>
